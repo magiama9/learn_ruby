53de7fc23fbde46d50f721ae5b0378ddbad81ca6
magiama9 <58517943+magiama9@users.noreply.github.com>
2020-03-03 13:57:33 -0500

    Start writing rake test

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,24 @@
 #write your code here
+
+def add(num1, num2)
+  return num1 + num2
+end
+
+def subtract(num1, num2)
+  return num1 - num2
+end
+
+def sum(arr)
+  return arr.reduce(0, :+)
+end
+
+def multiply(*args)
+  return args.reduce(:*)
+end
+
+def power(num1, num2)
+  return num1 ** num2
+end
+
+def factorial(num)
+  answer = 1
