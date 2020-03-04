f9c1e5102687b81d66ba928e2c28f0c026fad111
magiama9 <58517943+magiama9@users.noreply.github.com>
2020-03-04 10:55:19 -0500

    rubocop

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,12 +1,14 @@
-#write your code here
+# frozen_string_literal: true
+
+# write your code here
 
 def ftoc(num)
-  return celsius = (num - 32) * 5/9
+  celsius = (num - 32) * 5 / 9
 end
 
 def ctof(num)
   float = num.to_f
-  return fahr = (float * 9/5) + 32.0
+  fahr = (float * 9 / 5) + 32.0
 end
 
 p ctof(37)
