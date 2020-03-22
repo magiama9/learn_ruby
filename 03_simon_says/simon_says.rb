c8a586fb6bdbb3e8a0161170023aa4f37c0b6800
magiama9 <samrandels@gmail.com>
2020-03-22 16:39:20 -0400

    Complete Simon Says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,34 @@
-#write your code here
+# frozen_string_literal: true
+
+# write your code here
+
+def echo(string)
+  string
+end
+
+def shout(string)
+  string.upcase
+end
+
+def repeat(string, times = 2)
+  ((string + ' ') * times).rstrip
+end
+
+def start_of_word(string, letters)
+  string[0, letters]
+end
+
+def first_word(string)
+  string.split(' ')[0]
+end
+
+def titleize(string)
+  array = string.split(' ').each_with_index do |word, index|
+    if index.zero? || word.length > 3
+      word.capitalize!
+    else
+      word
+    end
+  end
+  array.join(" ")
+end
